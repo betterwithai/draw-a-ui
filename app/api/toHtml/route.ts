2f62f926c3fb146b8aa29ed25698a1b6ca5d766e
Aleksei Balandin <alexbalandi@gmail.com>
2023-11-16 19:49:57 +0200

    disable linter for line

--- app/api/toHtml/route.ts
+++ app/api/toHtml/route.ts
@@ -138,6 +138,7 @@ export type GPT4VCompletionRequest = {
 }
 
 // API route handler
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async function (req: NextApiRequest, res: NextApiResponse) {
     // Check if it's a POST request
     if (req.method !== 'POST') {
