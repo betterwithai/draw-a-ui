42d08cc7bec9d9afbcc7e1962a541285cfd497ac
Aleksei Balandin <alexbalandi@gmail.com>
2023-11-16 17:43:23 +0200

    brr

---
+++ app/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const next = require('next');
+const auth = require('basic-auth');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const basicAuthCheck = (req, res, next) => {
+    const user = auth(req);
+    const USERNAME = process.env.BASIC_AUTH_USERNAME;
+    const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
+
+    if (!user || user.name !== USERNAME || user.pass !== PASSWORD) {
+        res.setHeader('WWW-Authenticate', 'Basic realm="Secure Area"');
+        res.status(401).send('Authentication required.');
+        return;
+    }
+    next();
+};
+
+app.prepare().then(() => {
+    const server = express();
+
+    // Apply basic auth check to all routes
+    server.use(basicAuthCheck);
+
+    server.all('*', (req, res) => {
+        return handle(req, res);
+    });
+
+    const PORT = process.env.PORT || 3000;
+    server.listen(PORT, err => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${PORT}`);
+    });
+});
